4ff411428d29eb9bc2905355f5dd1935d0fd22a0
Davi Maciel <davimaciel077@gmail.com>
2024-06-14 13:41:11 -0300

    commit

---
+++ aula01.py
@@ -0,0 +1,11 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de
+# classes.
+
+string = 'davi' #str
+print(string.upper())
+print(isinstance(string, str))
