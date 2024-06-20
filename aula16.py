468b7fcf0c4bc4320d6775269cd72942a5577b88
Davi Maciel <davimaciel077@gmail.com>
2024-06-20 10:58:14 -0300

    commit

---
+++ aula16.py
@@ -0,0 +1,33 @@
+# Relações entre classes: associação, agregação e composição
+# Composição é uma especialização da agregação.
+# Mas nela, quando o objeto "pai" for apagado, todas
+# as referências dos objetos filhos também são
+# apagadas.
+class Cliente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.enderecos = []
+
+    def inserir_enderecos(self, rua, numero):
+        self.enderecos.append(Endereco(rua, numero))
+
+    def listar_enderecos(self):
+        for endereco in self.enderecos:
+            print(endereco.rua, endereco.numero)
+
+
+class Endereco:
+    def __init__(self, rua, numero):
+        self.rua = rua
+        self.numero = numero
+
+    def __del__(self):
+        print('APAGANDO', self.rua, self.numero)
+
+cliente = Cliente('davi')
+cliente.inserir_enderecos('faz. curupati', 0)
+cliente.inserir_enderecos('rua B', 8374)
+cliente.listar_enderecos()
+
+del cliente
+print('##################################')
