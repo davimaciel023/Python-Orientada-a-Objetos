b39e8446f5cdca3146a13ee81f08c96b55ef6143
Davi Maciel <davimaciel077@gmail.com>
2024-06-20 10:11:29 -0300

    commit

---
+++ aula13.py
@@ -0,0 +1,33 @@
+# Encapsulamento (modificadores de acesso: public, protected, private)
+# Python NÃO TEM modificadores de acesso
+# Mas podemos seguir as seguintes convenções
+#   (sem underline) = public
+#       pode ser usado em qualquer lugar
+# _ (um underline) = protected
+#       não DEVE ser usado fora da classe
+#       ou suas subclasses.
+# __ (dois underlines) = private
+#       "name mangling" (desfiguração de nomes) em Python
+#       _NomeClasse__nome_attr_ou_method
+#       só DEVE ser usado na classe em que foi
+#       declarado.
+from functools import partial
+
+
+class Foo:
+    def __init__(self):
+        self.public = 'isso é publico'
+        self._protected = 'isso é protegido'
+        self.__private = 'isso é private'
+
+    def _metodo_protected(self):    
+        return '_metodo_protected'
+    
+    def __metodo_private(self):    
+        return '__metodo_private'
+
+f = Foo()
+print(f._protected)
+print(f._metodo_protected())
+print(f._Foo__metodo_private())
+# print(f.public)
