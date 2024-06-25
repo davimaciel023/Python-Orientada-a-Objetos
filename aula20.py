dbea73b9500b06cad284924f8fffc137ca3e1766
Davi Maciel <davimaciel077@gmail.com>
2024-06-25 14:36:14 -0300

    commit

--- aula20.py
+++ aula20.py
@@ -3,3 +3,30 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+# class MinhaString(str):
+#     def upper(self):
+#         print('CHAMOU UPPER')
+#         return super().upper()
+
+
+# string = MinhaString("Davi")
+# print(string.upper())
+
+class A:
+    atributo_a = 'valor'
+    def metodo(self):
+        print('A')
+
+class B(A):
+    atributo_b = 'valor'
+    def metodo(self):
+        print('B')
+
+class C(B):
+    atributo_c = 'valor'
+    def metodo(self):
+        super().metodo()
+        print('C')
+
+c = C()
+c.metodo()
