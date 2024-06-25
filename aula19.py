37a1b4f2a692cd04088cd43b4a0b12a2494951c1
Davi Maciel <davimaciel077@gmail.com>
2024-06-25 14:13:01 -0300

    commit

--- aula19.py
+++ aula19.py
@@ -8,3 +8,16 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar_nome_class(self):
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+
+class Cliente(Pessoa):
+    ...
+
+c1 = Cliente('Davi', 'Maciel')
+c1.falar_nome_class()
