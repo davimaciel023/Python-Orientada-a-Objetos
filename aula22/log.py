acc409f85cf065fe5be61775b227cef2b7f973d2
Davi Maciel <davimaciel077@gmail.com>
2024-06-28 07:09:54 -0300

    commit

--- aula22/log.py
+++ aula22/log.py
@@ -1,13 +1,27 @@
 # Abstração
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
     
+
+    def log_error(self, msg):
+        return self.log_error(f"Erro: {msg}")
+
+    def log_success(self, msg):
+        return self.log_error(f"Success: {msg}")
+
+    
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} {self.__class__.__name__}')
 
 
 if __name__ == '__main__':
     l = LogFileMixin()
-    l.log('qualquer coisa')
+    l.log_error('qualquer coisa')
+    l.log_success('Sucesso')
