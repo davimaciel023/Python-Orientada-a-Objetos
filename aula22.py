6f3721c63721a651576fab79f2d0d08e47b42afc
Davi Maciel <davimaciel077@gmail.com>
2024-07-05 07:18:18 -0300

    commit

--- aula22.py
+++ aula22.py
@@ -14,3 +14,22 @@
 # É possível criar @property @setter @classmethod
 # @staticmethod e @method como abstratos, para isso
 # use @abstractmethod como decorator mais interno.
+from abc import ABC, abstractmethod
+
+class Log(ABC):
+    @abstractmethod
+    def _log(self, msg): ...
+    
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+l = LogPrintMixin()
+l.log_error('oi')
