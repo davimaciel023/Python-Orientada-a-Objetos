bd97238da0d5e32c0902f021e4819895407d15a6
Davi Maciel <davimaciel077@gmail.com>
2024-07-09 15:51:16 -0300

    commit

--- aula27.py
+++ aula27.py
@@ -15,3 +15,20 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'{self.x}, {self.y}'
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'{class_name}(x= {self.x}, y= {self.y})'
+
+p1 = Ponto(1, 2)
+p2 = Ponto(234, 243)
+print(p1)
+print(repr(p2))
+print(f'{p2!r}')
