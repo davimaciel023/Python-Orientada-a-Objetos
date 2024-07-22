e92a01c5e03d7fea97952b7935211015f5143af4
Davi Maciel <davimaciel077@gmail.com>
2024-07-22 10:38:06 -0300

    commit

--- aula30.py
+++ aula30.py
@@ -18,3 +18,24 @@
 # Ex:
 # with open('aula149.txt', 'w') as arquivo:
 #     ...
+class MyOpen:
+    def __init__(self, caminho_arquivo, modo) -> None:
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = modo
+        self._arquivo = None
+
+    def __enter__(self):
+        print("Abrindo arquivo")
+        self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8')
+        return self._arquivo
+    
+    def __exit__(self, class_exception, exception_, traceback_):
+        print('EXIT')  
+        self._arquivo.close()
+
+
+with MyOpen('aula30.txt', 'w') as arquivo:
+    arquivo.write('linha 1\n')
+    arquivo.write('linha 2\n')
+    arquivo.write('linha 3\n')
+    print('With', arquivo)
