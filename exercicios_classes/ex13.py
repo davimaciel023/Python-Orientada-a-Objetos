a3fa0f675a0bedb75a9b6a54ddcfac79ef0bcb31
Davi Maciel <davimaciel077@gmail.com>
2024-07-23 21:01:32 -0300

    commit

--- exercicios_classes/ex13.py
+++ exercicios_classes/ex13.py
@@ -1,3 +1,8 @@
 """
 Classe Funcionário: Implemente a classe Funcionário. Um empregado tem um nome (um string) e um salário(um double). Escreva um construtor com dois parâmetros (nome e salário) e métodos para devolver nome e salário. Escreva um pequeno programa que teste sua classe.
 """
+class Funcionario:
+    def __init__(self, empregado, salario):
+        self.empregado = empregado
+        self.salario = salario
+        
