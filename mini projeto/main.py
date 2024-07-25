431a42b55af4a6ca7435ab8d663bcfa1551ac245
Davi Maciel <davimaciel077@gmail.com>
2024-07-24 21:00:29 -0300

    commit

--- mini projeto/main.py
+++ mini projeto/main.py
@@ -1,12 +1,5 @@
 """
-Criar uma classe com sistema de login automatizado, que peça algumas informações ao usuário sobre o currículo
-Pedir: Nome, idade, objetivo, experiencia
+Criar uma classe com sistema de login automatizado
 """
-class Curriculo:
-    def __init__(self, nome, idade, objetivo, experiencia):
-        self._nome = nome
-        self._idade = idade
-        self._objetivo = objetivo
-        self._experiencia = experiencia
+import json
 
-    
