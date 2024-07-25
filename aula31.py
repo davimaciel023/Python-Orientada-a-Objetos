9b9eb7004ea437d94939c16363de7e0222be06fd
Davi Maciel <davimaciel077@gmail.com>
2024-07-24 21:20:53 -0300

    commit

--- aula31.py
+++ aula31.py
@@ -3,14 +3,16 @@ from contextlib import contextmanager
 
 @contextmanager
 def my_open(caminho_arquivo, modo):
-    print('Abrindo arquivo')
-    arquivo = open(caminho_arquivo, modo, encoding='utf8')
-    yield arquivo
-    print('Fechando arquivo')
-    arquivo.close()
+    try:
+        print('Abrindo arquivo')
+        arquivo = open(caminho_arquivo, modo, encoding='utf8')
+        yield arquivo
+    finally:
+        print('Fechando arquivo')
+        arquivo.close()
 
 with my_open('aula31.txt', 'w') as arquivo:
     arquivo.write('linha 1\n')
-    arquivo.write('linha 2\n')
+    arquivo.write('linha 2\n', 13)
     arquivo.write('linha 3\n')
     print('With', arquivo)
