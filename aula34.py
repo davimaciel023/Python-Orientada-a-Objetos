3e764ddd86247d6c4ed8c2cde018a37e7ce9cea9
Davi Maciel <davimaciel077@gmail.com>
2024-08-20 21:04:38 -0300

    commit

--- aula34.py
+++ aula34.py
@@ -2,3 +2,12 @@
 # callable é algo que pode ser executado com parênteses
 # Em classes normais, __call__ faz a instância de uma
 # classe "callable"
+class CallMe:
+    def __init__(self, phone):
+        self.phone = phone
+    
+    def __call__(self, nome, *args, **kwargs):
+        print(nome, 'está chamando', self.phone)
+
+call1 = CallMe('9823948223')
+call1('Davi')
