51436dfb05c253e790a1f46020cbbbea845575d3
Davi Maciel <davimaciel077@gmail.com>
2024-09-06 11:46:46 -0300

    commit

--- criptografando.py
+++ criptografando.py
@@ -1,17 +1,16 @@
-def criptografando(senha, casas):
-    criptografia = ''
-    for i in range(len(senha)):
-        if senha[i] != ' ':
-            if (len(strings) + casas) >= 26:
-                for j in range():
-                    ...
-            else:
-                criptografia += strings[i+casas]
-        else:
-            criptografia += senha[i]
-    print(criptografia)
-
-senha = input("Digite uma frase: ").lower()
-casas = int(input("Quantas casa quer pular ? "))
-strings = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-criptografando(senha, casas)
+class Criptografia:
+    def __init__(self, senha):
+        self.criptografia = senha
+    
+    def criptogrando(senha):
+        string = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+        criptografada = ''
+        posicao = ()
+        for i in range(len(senha)):
+            if senha[i] != ' ':
+                for j in range(senha):
+                    for k in range(string):
+                        if senha[i] == string[k]:
+                            posicao += k
+                        else:
+                            ...
