97df079918f3ee8887cff9369d7aea4b384e81f1
Davi Maciel <davimaciel077@gmail.com>
2024-10-05 16:55:41 -0300

    commit

--- criptografando.py
+++ criptografando.py
@@ -1,16 +1,32 @@
 class Criptografia:
-    def __init__(self, senha):
-        self.criptografia = senha
+    def __init__(self, frase):
+        self.criptografia = frase
     
-    def criptogrando(senha):
-        string = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-        criptografada = ''
+    def criptografando(frase, pular):
+        self.string = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", ' ')
+        self.criptografada = ''
         posicao = ()
-        for i in range(len(senha)):
-            if senha[i] != ' ':
-                for j in range(senha):
-                    for k in range(string):
-                        if senha[i] == string[k]:
+        for i in range(len(frase)):
+            if frase[i] != ' ':
+                for j in range(len(frase)):
+                    for k in range(len(self.string)):
+                        if frase[j] == self.string[k]:
                             posicao += k
                         else:
                             ...
+                
+                for l in range(len(frase)):
+                    if (posicao[l] + pular) <= 26:
+                        self.criptografada += self.string[posicao[l]]
+
+                    else:
+                        self.criptografada += self.string[(26-(posicao[l] - pular))]
+            else: 
+                ...
+            print(self.criptografada)
+    
+frase = input('Digite uma frase: ').lower()
+pular = int(input('Quantas casas que pular? '))
+
+c = Criptografia(frase)
+c.criptografando(frase, pular)
